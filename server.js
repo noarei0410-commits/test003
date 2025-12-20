e00b13e183427a672c9c049e53e2bb8ee7ba0324
noarei0410-commits <noa.rei0410@gmail.com>
2025-12-21 04:27:44 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,8 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// サーバー側の状態管理
 let deck = [];
-let fieldState = {}; // { cardId: { number, x, y, zIndex, isFaceUp } }
+let fieldState = {}; 
 
 function initDeck() {
     deck = [];
@@ -22,52 +21,39 @@ function initDeck() {
 initDeck();
 
 io.on('connection', (socket) => {
-    // 1. 新規接続した人に、現在のフィールドの状態をすべて送る
-    socket.emit('init', { 
-        id: socket.id, 
-        fieldState: fieldState 
-    });
+    socket.emit('init', { id: socket.id, fieldState: fieldState });
     io.emit('deckCount', deck.length);
 
-    // カードを引く
     socket.on('drawCard', () => {
         if (deck.length > 0) {
             const num = deck.pop();
-            const cardData = {
-                id: uuidv4(),
-                number: num
-            };
-            // 引いた本人にだけカードを送る（この時点ではフィールド未登録）
+            const cardData = { id: uuidv4(), number: num };
             socket.emit('receiveCard', cardData);
             io.emit('deckCount', deck.length);
         }
     });
 
-    // 移動と状態の保存
     socket.on('moveCard', (data) => {
-        // フィールドの状態を更新・保存
-        if (!fieldState[data.id]) fieldState[data.id] = {};
         fieldState[data.id] = {
             ...fieldState[data.id],
-            x: data.x,
-            y: data.y,
-            zIndex: data.zIndex,
-            number: data.number // 数字も保存しておく
+            x: data.x, y: data.y, zIndex: data.zIndex, number: data.number
         };
         socket.broadcast.emit('cardMoved', data);
     });
 
-    // 裏返しの保存
     socket.on('flipCard', (data) => {
-        if (fieldState[data.id]) {
-            fieldState[data.id].isFaceUp = data.isFaceUp;
-        }
+        if (fieldState[data.id]) fieldState[data.id].isFaceUp = data.isFaceUp;
         socket.broadcast.emit('cardFlipped', data);
     });
 
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
+    // 【新機能】手札に戻ったカードをフィールドデータから削除
+    socket.on('returnToHand', (data) => {
+        delete fieldState[data.id];
+        // 他のプレイヤーに「このカードを消して」と通知
+        socket.broadcast.emit('cardRemoved', { id: data.id });
     });
+
+    socket.on('disconnect', () => { console.log('User disconnected'); });
 });
 
 const PORT = process.env.PORT || 3000;
