5ae3786278bbaaddd7efa5f79e41afdf35948f4b
noarei0410-commits <noa.rei0410@gmail.com>
2025-12-21 04:53:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,44 +13,46 @@ app.use(express.static(path.join(__dirname, 'public')));
 let deck = [];
 let fieldState = {}; 
 
-function initDeck() {
-    deck = [];
-    for (let i = 1; i <= 52; i++) deck.push(i);
-    deck.sort(() => Math.random() - 0.5);
-}
-initDeck();
-
 io.on('connection', (socket) => {
     socket.emit('init', { id: socket.id, fieldState: fieldState });
     io.emit('deckCount', deck.length);
 
+    // デッキリストを設定してシャッフル
+    socket.on('setDeck', (list) => {
+        deck = list.map(name => ({
+            id: uuidv4(),
+            name: name.trim()
+        }));
+        // シャッフル
+        for (let i = deck.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+        io.emit('deckCount', deck.length);
+        console.log("Deck initialized with " + deck.length + " cards.");
+    });
+
     socket.on('drawCard', () => {
         if (deck.length > 0) {
-            const num = deck.pop();
-            const cardData = { id: uuidv4(), number: num };
+            const cardData = deck.pop();
             socket.emit('receiveCard', cardData);
             io.emit('deckCount', deck.length);
         }
     });
 
     socket.on('moveCard', (data) => {
-        fieldState[data.id] = {
-            ...fieldState[data.id],
-            x: data.x, y: data.y, zIndex: data.zIndex, number: data.number
-        };
+        fieldState[data.id] = { ...fieldState[data.id], ...data };
         socket.broadcast.emit('cardMoved', data);
+    });
+
+    socket.on('returnToHand', (data) => {
+        delete fieldState[data.id];
+        socket.broadcast.emit('cardRemoved', { id: data.id });
     });
 
     socket.on('flipCard', (data) => {
         if (fieldState[data.id]) fieldState[data.id].isFaceUp = data.isFaceUp;
         socket.broadcast.emit('cardFlipped', data);
-    });
-
-    // 【新機能】手札に戻ったカードをフィールドデータから削除
-    socket.on('returnToHand', (data) => {
-        delete fieldState[data.id];
-        // 他のプレイヤーに「このカードを消して」と通知
-        socket.broadcast.emit('cardRemoved', { id: data.id });
     });
 
     socket.on('disconnect', () => { console.log('User disconnected'); });
