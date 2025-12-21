05408475402f9ca30914298a77805dfb679a0206
noarei0410-commits <noa.rei0410@gmail.com>
2025-12-21 23:46:25 +0900

    Update server.js

--- server.js
+++ server.js
@@ -82,4 +82,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running: http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
