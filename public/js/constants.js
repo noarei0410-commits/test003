9e8296a6528185a5e76f914c0edea83c0eb96818
noarei0410-commits <noa.rei0410@gmail.com>
2025-12-24 15:56:04 +0900

    Update constants.js

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,10 +1,7 @@
 const socket = io();
-
-// ゾーン定義
 const STAGE_ZONES = ['collab', 'center', 'oshi', 'back1', 'back2', 'back3', 'back4', 'back5'];
 const COLORS = { '白': 'white', '緑': 'green', '赤': 'red', '青': 'blue', '黄': 'yellow', '紫': 'purple' };
 
-// グローバル状態
 let MASTER_CARDS = [], OSHI_LIST = [], AYLE_MASTER = [];
 let mainDeckList = [], cheerDeckList = [], selectedOshi = null;
 let myRole = 'spectator';
@@ -13,7 +10,6 @@ let currentStack = [];
 let startX = 0, startY = 0, offsetX = 0, offsetY = 0, maxZIndex = 1000;
 let potentialZoomTarget = null;
 
-// HTML要素参照
 const field = document.getElementById('field');
 const handDiv = document.getElementById('hand');
 const setupModal = document.getElementById('setup-modal');
